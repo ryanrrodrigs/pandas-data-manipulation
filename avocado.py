5dcde72504b1785a2563d7c3fce9206eba9e2142
ryanrrodrigs <154026064+ryanrrodrigs@users.noreply.github.com>
2024-06-26 23:11:03 -0700

    Update avocado.py

--- avocado.py
+++ avocado.py
@@ -19,5 +19,14 @@ nb_sold_by_date.plot(kind="line")
 # Scatter plot of avg_price vs. nb_sold with title
 avocados.plot(x="nb_sold", y="avg_price", kind="scatter", title="Number of avocados sold vs. average price")
 
+# Modify bins to 20
+avocados[avocados["type"] == "conventional"]["avg_price"].hist(alpha=0.5, bins=20)
+
+# Modify bins to 20
+avocados[avocados["type"] == "organic"]["avg_price"].hist(alpha=0.5, bins=20)
+
+# Add a legend
+plt.legend(["conventional", "organic"])
+
 # Show the plot
 plt.show()
