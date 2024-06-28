4797e3470bd38c1778ee7cc285d7036399f764b5
ryanrrodrigs <154026064+ryanrrodrigs@users.noreply.github.com>
2024-06-27 19:39:42 -0700

    Update dictionaries & lists for pandas.py

--- dictionaries & lists for pandas.py
+++ dictionaries & lists for pandas.py
@@ -4,6 +4,13 @@ avocados_list = [
     {"date": "2019-11-10", "small_sold": 10717154, "large_sold": 8561348},
 ]
 
+# Create a dictionary of lists with new data alternative method
+avocados_dict = {
+  "date": ["2019-11-17", "2019-12-01"],
+  "small_sold": [10859987, 9291631],
+  "large_sold": [7674135, 6238096]
+}
+
 # Convert list into DataFrame
 avocados_2019 = pd.DataFrame(avocados_list)
 
